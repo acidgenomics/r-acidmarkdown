13ff839223a0223a1f3e609f419292d65b45bd92
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 09:22:55 -0400

    Move imports to package.R

--- R/package.R
+++ R/package.R
@@ -4,3 +4,19 @@
 #'
 #' @keywords internal
 "_PACKAGE"
+
+
+
+## S4 generics =================================================================
+
+#' @importFrom AcidGenerics markdownPlots markdownTables
+NULL
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom goalie areSameLength assert hasLength hasNames isADirectory isAny
+#' isCharacter isFlag isHeaderLevel isInstalled isString
+#' @importFrom methods setMethod signature
+NULL
